e5e0ac9b032a8315fab8920d105449ca662de45d
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-05-26 10:48:33 +0200

    ci stuff

--- test+readme.py
+++ test+readme.py
@@ -1,11 +1,11 @@
 from modules import modules
 
 with open("README.md", "r+", encoding='utf-8') as fp:
-    before_modules, modules, after_modules = fp.read().split("<!--modules-->")
+    before_modules, _, after_modules = fp.read().split("<!--modules-->")
     fp.seek(0)
     fp.write(before_modules)
     fp.write("<!--modules-->\n")
-    for module in modules:
-        fp.write(f"- {module.name}: \n{module.summary}\n\n")
+    for module in sorted(modules):
+        fp.write(f"- **{module.name}**: {module.summary}\n")
     fp.write("\n<!--modules-->")
     fp.write(after_modules)

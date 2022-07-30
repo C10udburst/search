6f1a8fdb4ad820d9d060f9fa38a9cf96401798f0
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-07-30 14:51:11 +0200

    update CSEs: ebooks, files, streaming

--- modules/google/__init__.py
+++ modules/google/__init__.py
@@ -15,7 +15,7 @@ google_modules = {
         "text",
         "texts"
     ], summary="Return gists."),
-    GoogleCSEModule("Ebooks", "003753031376654422446:szjag5vbefo", [
+    GoogleCSEModule("Ebooks", ["003753031376654422446:szjag5vbefo", "006516753008110874046:s9ddesylrm8", "e9657e69c76480cb8", "c46414ccb6a943e39"], [
         "books",
         "ebooks",
         "book",
@@ -37,11 +37,23 @@ google_modules = {
         "file",
         "download"
     ], summary="Search for files in google drive"),
-    GoogleCSEModule("Roms", ["017627112597466747210:d_mvqq9pgve"], [
+    GoogleCSEModule("Roms", "017627112597466747210:d_mvqq9pgve", [
         "roms",
         "emulation",
         "consoles",
         "nintendo",
         "games"
     ], summary="Search for console roms."),
+    GoogleCSEModule("Files", ["006516753008110874046:1ugcdt3vo7z", "006516753008110874046:reodoskmj7h"], [
+        "files",
+        "file",
+        "dl",
+        "download"
+    ], summary="Search for files"),
+    GoogleCSEModule("Streaming", ["006516753008110874046:cfdhwy9o57g", "006516753008110874046:o0mf6t-ugea", "98916addbaef8b4b6", "0199ade0b25835f2e"], [
+        "streaming",
+        "movies",
+        "series",
+        "tv"
+    ], summary="Search for movie or tv series streaming"),
 }
